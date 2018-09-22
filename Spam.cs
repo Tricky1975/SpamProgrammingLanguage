20ba1d9693a2963f881783810f52a151cbf488c3
Tricky1975 <facebook@tbbs.nl>
2018-09-22 10:06:33 +0200

    Special vUpdate

--- Spam.cs
+++ Spam.cs
@@ -1,26 +1,27 @@
-/*
-Rem
-  Spam.bmx
-  Spam Programming Language
-  version: 17.09.09
-  Copyright (C) 2017 Jeroen P. Broks
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-End Rem
-
-This is a translation from BlitzMax to C#
-*/
+// Lic:
+// 	Spam Programming Language
+// 	Esotoric Language to make code look like spam
+// 	
+// 	
+// 	
+// 	(c) Jeroen P. Broks, 2018, All rights reserved
+// 	
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
+// 		
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 		
+// 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
+// 	to the project the exceptions are needed for.
+// Version: 18.09.22
+// EndLic
 
 //Strict
 //Framework tricky_units.PrefixSuffix
@@ -244,8 +245,8 @@ End Function  itmap.add "CASH",it_cash ' decrease pointer
         static public void Main(string[] AAppArgs)
         {
             AppArgs = AAppArgs;
-            MKL.Version("Spam Programming Language - Spam.bmx", "17.09.09");
-            MKL.Lic("Spam Programming Language - Spam.bmx", "ZLib License");
+            MKL.Version("Spam Programming Language - Spam.cs","18.09.22");
+            MKL.Lic    ("Spam Programming Language - Spam.cs","GNU General Public License 3");
             if (AppArgs.Length < 1)
             {
                 //Print("Spam Programming Language - version "+MKL.NewestVersion());
